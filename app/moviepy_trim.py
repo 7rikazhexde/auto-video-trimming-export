3bf9ab19b2dbd090d63cea31ea1cbbb55ed15431
7rikaz_h785 <7rikaz.h785.stat2ltas41lcijad@gmail.com>
2023-01-29 22:50:10 +0900

    Comment Modified.

--- app/moviepy_trim.py
+++ app/moviepy_trim.py
@@ -1,19 +1,19 @@
 from moviepy.editor import *
 
 def trim_video(file_path,export_file,start,end,fps):
-    # ビデオのカット開始
+    # Start cutting video
     video_clip = VideoFileClip(file_path).subclip(start, end)
     vide_clip_resized = video_clip.resize(height=360) 
     vide_clip_resized.write_videofile(export_file,fps,codec='libx264',audio_codec='aac',threads=4)
 
 if __name__ == '__main__':
-    # 編集したい動画のパス
+    # Media path
     file_path = './media/test.mp4'
-    # 編集後のファイル保存先のパス
+    # Export path
     export_file = './media/test_trim.mp4'
-    # 切り出し開始時刻。秒で表現
+    # Cutout start time (sec)
     start = 50
-    # 切り出し終了時刻。同じく秒で表現
+    # Cutout end time (sec)
     end = 250
     fps = 30
     trim_video(file_path, export_file, start, end, fps)
